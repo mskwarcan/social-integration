0cd68f94a8df89be50d745d2880a1c73266a90a9
Mike Skwarcan <mikeskwarcan@Mike-Skwarcans-iMac.local>
2011-02-11 15:49:24 -0600

    twitter test

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,31 +1,30 @@
 class HomeController < ApplicationController
   require "rubygems"
-  require "twitter_oauth"
+  require 'twitter_oauth'
   
   def index
     
   end
   
   def register
-    @twitter_client = TwitterOAuth::Client.new(
-      :consumer_key     => '4xm5KPQDlTw3RlQKQuO8qQ',
-      :consumer_secret  => 'AKvzYoI8WERZghSmK9J3jeY9YnCmcevWGBvdyK5A'
-    )
-    request_token = @twitter_client.request_token(:oauth_callback => 'http://socialintegration.heroku.com')
-    
+    @user = session[:user]
+    unless @user.twitter_authd?(@user)
+          client = TwitterOAuth::Client.new(
+              :consumer_key => '4xm5KPQDlTw3RlQKQuO8qQ',
+              :consumer_secret => 'AKvzYoI8WERZghSmK9J3jeY9YnCmcevWGBvdyK5A'
+          )
+          request_token = client.request_token(:oauth_callback => 'http://socialintegration.heroku.com') 
+    end
   end
   
   def oauth_callback
-     @twitter_client = TwitterOAuth::Client.new(
-     :consumer_key     => '4xm5KPQDlTw3RlQKQuO8qQ',
-     :consumer_secret  => 'AKvzYoI8WERZghSmK9J3jeY9YnCmcevWGBvdyK5A'
+     access_token = client.authorize(
+       request_token.token,
+       request_token.secret,
+       :oauth_verifier => params[:oauth_verifier]
      )
 
-     @twitter_access_token = @twitter_client.authorize(
-       session[:request_token],
-       session[:request_token_secret],
-        auth_verifier =>params[:oauth_verifier]
-     )
+     client.update('checking out the twitter_oauth library') # sends a twitter status update
    end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,15 @@
 class User < ActiveRecord::Base
   validates :username,  :presence => true,
-                        :length => { :minimum => 6}
+                        :length => { :minimum => 6},
+                        :uniqueness => true
   validates :password,  :presence => true,
                         :length => { :minimum => 6}
   validates :name,  :presence => true
+  
+  def twitter_authd?(user)
+    @user = User.first(:conditions => {:username => user.username})
+    if @user.twitter_token
+      return true
+    end
+  end
 end
