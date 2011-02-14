77095f7bf8368d03ba05319b0f1c09fedc0ef8ca
Mike Skwarcan <mikeskwarcan@Mike-Skwarcans-iMac.local>
2011-02-14 11:45:07 -0600

    twitter

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,6 +9,8 @@ class HomeController < ApplicationController
   def register
     @user = session[:user]
     
+    puts "User created"
+    
     client = TwitterOAuth::Client.new(
     :consumer_key => '4xm5KPQDlTw3RlQKQuO8qQ',
     :consumer_secret => 'AKvzYoI8WERZghSmK9J3jeY9YnCmcevWGBvdyK5A',
@@ -16,11 +18,18 @@ class HomeController < ApplicationController
     :secret => @user.twitter_secret
     )
     
+    puts "Client created"
+    
     unless @user.twitter_authd?(@user)
-      request_token = client.request_token( :oauth_callback => 'http://socialintegration.heroku.com' )
+      puts "Client not authorized"
+      request_token = client.request_token( :oauth_callback => 'http%3A%2F%2Fsocialintegration.heroku.com%2F/twitter_oauth' )
+      puts "Get Tokens"
       @user.twitter_token = request_token.token
+      puts request_token.token
       @user.twitter_secret = request_token.secret
+      puts request_token.secret
       @user.save(false)
+      puts "Save"
       redirect_to request_token.authorize_url
     end
     
