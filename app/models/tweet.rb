e2985760f33863c3f7bb7c8d9906e907e02469c8
Mike Skwarcan <mikeskwarcan@Mike-Skwarcans-iMac.local>
2011-02-15 09:56:44 -0600

    Twitter Tests

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,4 +1,18 @@
 class Tweet < ActiveRecord::Base
+
+  MY_APPLICATION_NAME = "Social Integretion"
+  
+  """Connect to the Twitter API and pull down the latest tweets"""
+  def self.get_latest(client)
+    tweets = client.statuses.user_timeline? :screen_name => MY_APPLICATION_NAME # hit the API
+    tweets.each do |t|
+      created = DateTime.parse(t.created_at)
+      # create the tweet if it doesn't already exist
+      unless Tweet.exists?(["created=?", created])
+        Tweet.create({:content => t.text, :created => created })
+       end
+    end
+  end
   
   private
   def self.client(user)
