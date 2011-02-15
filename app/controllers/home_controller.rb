e5bfe51da1f0ca3785ef1ce697025e201a09200a
Mike Skwarcan <mikeskwarcan@Mike-Skwarcans-iMac.local>
2011-02-15 09:34:11 -0600

    Twitter tests

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -29,6 +29,8 @@ class HomeController < ApplicationController
     )
     
     request_token = client.request_token( :oauth_callback => 'http://socialintegration.heroku.com/twitter_oauth' )
+    puts request_token.token
+    puts request_token.secret
     redirect_to request_token.authorize_url
     
   end
@@ -36,18 +38,19 @@ class HomeController < ApplicationController
   def twitter_oauth
     @user = session[:user]
     
+    puts "Twitter Oauth"
     client = TwitterOAuth::Client.new(
     :consumer_key => 'AsAuRyGZP73Pr6863VS4Pg',
     :consumer_secret => '2bNk5FCvT39HzFN8eHPdbpFNKkaJHnxpQkZ6xz17sY'
     )
-    
-    request_token = client.request_token( :oauth_callback => 'http://socialintegration.heroku.com/twitter_oauth' )
     
      access_token = client.authorize(
        request_token.token,
        request_token.secret,
        :oauth_verifier => params[:oauth_verifier]
      )
+     
+     puts access_token
      
      @user.twitter_token = access_token.token
      @user.twitter_secret = access_token.secret
